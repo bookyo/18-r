bacd1e62f8ec206028081bc52a52472de4d274b8
golang <mwm0022@126.com>
2017-09-24 22:52:03 -0700

    add lazyload

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   var summary = $(".summary").html();
   var maxwidth = 100;
+  $("img.lazy").lazyload({
+    effect : "fadeIn"
+  });
   if( summary && summary.length > maxwidth) {
      $(".summary").html($(".summary").text().substring(0,maxwidth)
      	+ "...<a class='more' href='javascript:;'>展开全部</a>");
