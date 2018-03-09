5c62fb21fba32f6baa0a4dbab6d4e9fde9870a20
bookyo <195996048@qq.com>
2018-03-09 20:19:09 +0800

    index add topics

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,38 @@
 var Movie = require('../models/movie');
 var Tag = require('../models/tag');
+var Topic = require('../models/topic');
 exports.index =  function(req, res, next) {
       var page = req.query.page > 0 ? req.query.page : 1;
       Movie.getMoviesByRedis(page, function(err, indexObj) {
         if(err) {
           console.log(err);
         }
-        res.render('index', { 
-          page: page,
-          pages: indexObj.pages,
-          user: req.session.user,
-          tags: req.tags,
-          hots: req.hots,
-          movies: indexObj.movies,
-          error: req.flash('error'),
-          success: req.flash('success').toString()
-        })
+        var perPage = 4;
+        Topic
+          .find()
+          .select('topic _id movies creator summary')
+          .populate('movies','title _id img')
+          .limit(perPage)
+          .skip(perPage * (page-1))
+          .sort('-meta.updateAt')
+          .exec(function(err, topics) {
+            if(err) {
+              console.log(err);
+            }
+            res.render('index', { 
+              page: page,
+              pages: indexObj.pages,
+              user: req.session.user,
+              tags: req.tags,
+              hots: req.hots,
+              topics: topics,
+              movies: indexObj.movies,
+              error: req.flash('error'),
+              success: req.flash('success').toString()
+            })
+
+          })
+        
       })
     
   }
